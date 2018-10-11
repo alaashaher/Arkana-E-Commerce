c06b0bce31d7c23bbd9151190fa3e3c089b3040d
unknown <alaashaher12@gmail.com>
2018-10-11 15:47:49 +0200

    map section

--- Arkana-E-Commerce/src/index.js
+++ Arkana-E-Commerce/src/index.js
@@ -18,3 +18,18 @@ $(function() {
       $(this).toggleClass('active');
   });
 });
+
+
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 30.045915,lng: 31.224290},
+          zoom: 13
+        });
+        var tribeca = {lat: 30.045915,lng: 31.224290};
+        var marker = new google.maps.Marker({
+          position: tribeca,
+          map: map,
+          title: 'First Marker!'
+        });
+      }
