5c16ce569baee587a5cbefcdafa0cd63247362c4
unknown <alaashaher12@gmail.com>
2018-10-11 15:26:39 +0200

    footer and logos section

--- Arkana-E-Commerce/src/index.js
+++ Arkana-E-Commerce/src/index.js
@@ -1,5 +1,4 @@
 import 'bootstrap';
-
 import './scss/index.scss';
 
 $('#alert').click(() => {
@@ -7,3 +6,15 @@ $('#alert').click(() => {
 });
 
 // Your jQuery code
+
+$(function() {
+  //Simple filter controls
+  $('.simplefilter li').click(function() {
+      $('.simplefilter li').removeClass('active');
+      $(this).addClass('active');
+  });
+  //Multifilter controls
+  $('.multifilter li').click(function() {
+      $(this).toggleClass('active');
+  });
+});
